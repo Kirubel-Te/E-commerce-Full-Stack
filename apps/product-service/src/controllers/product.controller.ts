9eb3cf62cab2b0fb593b246f779bd84872f2983a
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-08-19 23:15:42 +0300

    product CRUD done

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -22,7 +22,68 @@ export const createProduct = async (req: Request, res: Response) =>{
 
 
 }
-export const deleteProduct = async (req: Request, res: Response) =>{}
-export const updateProduct = async (req: Request, res: Response) =>{}
-export const getProduct = async (req: Request, res: Response) =>{}
-export const getAllProduct = async (req: Request, res: Response) =>{}
+export const deleteProduct = async (req: Request, res: Response) =>{
+    const {id} = req.params
+    const deletedProduct = await prisma.product.delete({
+        where:{
+            id:Number(id)
+        }
+    })
+    res.status(200).json(deletedProduct)
+}
+
+export const updateProduct = async (req: Request, res: Response) =>{
+    const {id} = req.params
+    const data: Prisma.ProductUpdateInput = req.body
+    const updatedProduct = await prisma.product.update({
+        where:{
+            id:Number(id)
+        },
+        data
+    })
+    res.status(200).json(updatedProduct)
+}
+
+export const getProduct = async (req: Request, res: Response) =>{
+    const {id} = req.params
+    const product = await prisma.product.findUnique({
+        where:{
+            id: Number(id)
+        }
+    })
+    if(!product){
+        return res.status(404).json({message:"Product not found"})
+    }
+    res.status(200).json(product)
+}
+
+export const getAllProduct = async (req: Request, res: Response) =>{
+    const {sort , category, search, limit} = req.query
+    const orderBy = (() => {
+        switch(sort){
+            case "asc":
+                return {price:Prisma.SortOrder.asc}
+            case "desc":
+                return {price:Prisma.SortOrder.desc}
+            case "oldest":
+                return{createdAt:Prisma.SortOrder.asc}
+            default:
+                return {createdAt:Prisma.SortOrder.desc}
+        }
+    }) ();
+
+    const products = await prisma.product.findMany({
+        where:{
+            category:{
+                slug: category as string
+            }, 
+            name:{
+                contains: search as string,
+                mode: "insensitive"
+            }
+        },
+        orderBy,
+        take: limit ? Number(limit) : undefined
+    })
+    res.status(200).json(products)
+}
