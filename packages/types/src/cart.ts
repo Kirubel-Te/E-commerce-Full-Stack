254cfe8b6d425d30ff0e86ae4df4cc5712272712
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-08-21 01:43:38 +0300

    stripe integrated and common types defined

---
+++ packages/types/src/cart.ts
@@ -0,0 +1,19 @@
+import type { Product } from "@repo/db";
+
+export type CartItemType = Product & {
+    quantity:number;
+    selectedSize: string;
+    selectedColor: string;
+}
+
+export type CartItemsType = CartItemType[]
+export type CartStoreStateType = {
+  cart: CartItemsType;
+  hasHydrated: boolean;
+};
+
+export type CartStoreActionsType = {
+  addToCart: (product: CartItemType) => void;
+  removeFromCart: (product: CartItemType) => void;
+  clearCart: () => void;
+};
---
+++ packages/types/src/products.ts
@@ -0,0 +1,43 @@
+import type{Product,Category} from "@repo/db"
+import type { CartItemType , CartItemsType} from "./cart"
+import {z} from "zod"
+
+export type StripeProductType = {
+    id:string,
+    name:string,
+    price:number
+}
+export type ProductType = Product
+export type CategoryType = Category 
+export type ProductsType = ProductType[];
+export const shippingFormSchema = z.object({
+  name: z.string().min(1, "Name is required!"),
+  email: z.email().min(1, "Email is required!"),
+  phone: z
+    .string()
+    .min(7, "Phone number must be between 7 and 10 digits!")
+    .max(10, "Phone number must be between 7 and 10 digits!")
+    .regex(/^\d+$/, "Phone number must contain only numbers!"),
+  address: z.string().min(1, "Address is required!"),
+  city: z.string().min(1, "City is required!"),
+});
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required!"),
+  cardNumber: z
+    .string()
+    .min(16, "Card Number is required!")
+    .max(16, "Card Number is required!"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/\d{2}$/,
+      "Expiration date must be in MM/YY format!"
+    ),
+  cvv: z.string().min(3, "CVV is required!").max(3, "CVV is required!"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
+
+
